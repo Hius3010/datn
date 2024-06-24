f41895969b9d99a4849625d99ab5385f403441a1
Hius3010 <hius3010@gmail.com>
2024-06-24 20:46:05 +0700

    final code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { PORT, URI } from "./api/config/index.js";
+import { PORT, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT, DATABASE_COLLECTION } from "./api/config/index.js";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -7,6 +7,7 @@ import { monitor } from './monitor.js';
 const app = express();
 
 // Ket noi mongodb
+const URI = `mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_COLLECTION}?authSource=admin`
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
